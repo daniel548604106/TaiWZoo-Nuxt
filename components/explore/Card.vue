4cb777f1e63efa65999167b429478c3bb1469015
hiskio-daniel <daniel.yeh@hiskio.com>
2020-12-24 15:33:50 +0800

    basic explore page

---
+++ components/explore/Card.vue
@@ -0,0 +1,20 @@
+
+<script>
+  export default {
+    props:{
+      img:{
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
+<template>
+  <div class="w-full max-w-110 rounded-10px shadow-md">
+    <img :src="img" alt="">
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/explore/index.vue
+++ pages/explore/index.vue
@@ -1,15 +1,49 @@
+<script>
+  import Card from '@/components/explore/Card.vue'
+  export default {
+    components:{
+      Card
+    },
+    data(){
+      return{
+        img: require('~/assets/logo.svg')
+      }
+    }
+  }
+</script>
 <template>
-  <div>
-    explore
+  <div class="w-full  px-10px">
+    <div class="tabs">
+      <ul>
+        <li class="active">Discover</li>
+        <li>Following</li>
+      </ul>
+    </div>
+    <div class="w-full max-w-600 mx-auto">
+      <Card :img="img" /> 
+    </div>
   </div>
 </template>
 
-<script>
-  export default {
 
-  }
-</script>
+<style lang="postcss" scoped>
 
-<style lang="scss" scoped>
+.tabs{
+  @apply  w-full max-w-400 mx-auto mt-80px mb-50px
+}
+
+ul{
+  @apply flex items-center justify-around;
+}
+
+li{
+  @apply text-20px
+}
+
+li:active{
+  @apply text-vue-main
+}
+
+
 
 </style>
